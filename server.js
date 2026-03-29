fc6bf6115d5b5c2d1806cd8807ca62c2be6d6f52
Hemanth-3456 <hemanth2007reddy@gmail.com>
2026-03-29 21:19:25 +0530

    final 2

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(__dirname));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'Obey.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(PORT, '0.0.0.0', () => {
